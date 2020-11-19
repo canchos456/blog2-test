592068a05114b6ddd29e7a09b36f4b272ac59048
CREMA <crema@FAC1s-MacBookAir.local>
2020-11-19 11:09:01 +0900

    Add association between link and User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_014805) do
+ActiveRecord::Schema.define(version: 2020_11_19_020613) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
